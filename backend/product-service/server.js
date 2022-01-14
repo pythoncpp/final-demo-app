4ce95c8e8004ead412245a7e0c6abaa43e539f00
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2022-01-14 12:20:08 +0530

    added a new route

--- backend/product-service/server.js
+++ backend/product-service/server.js
@@ -5,6 +5,10 @@ const app = express()
 app.use(cors('*'))
 app.use(express.json())
 
+app.use('/', (request, response) => {
+  response.send('welcome to the product service')
+})
+
 // add the routers
 const routerProduct = require('./routes/product')
 app.use('/product', routerProduct)
